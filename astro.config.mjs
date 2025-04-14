0bc073646a190c482ea83c15889bdb454b35a384
Rishi Raj Jain <jain71000@gmail.com>
2025-04-14 12:13:01 +0530

    add test case

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
+// import node from '@astrojs/node'
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel(), // node({mode:'standalone'}),
   vite: {
     ssr: {
       external: ['node:buffer', 'node:path', 'node:fs', 'node:os', 'node:crypto'],
